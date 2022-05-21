bf26fb827cf94d629e5b1c8e76545ada7566ad16
elad7290 <elad7290@gmail.com>
2022-05-21 17:03:32 +0300

    test

--- Whatsapp-Clone-api/Hubs/ChatHub.cs
+++ Whatsapp-Clone-api/Hubs/ChatHub.cs
@@ -4,5 +4,9 @@ namespace Whatsapp_Clone_api.Hubs
 {
     public class ChatHub: Hub
     {
+        public async Task UpdateMessages()
+        {
+            Console.WriteLine("hi chen");
+        }
     }
 }
